e4993aa8524922c89e16cf7f3013c44b6828dc8f
Ashish Cherian <ashishcherian15@gmail.com>
2026-09-19 19:01:40 +0530

    Phase 0: Repo hygiene, dependency alignment, and baseline security hardening

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_ashish_stash_StashApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.ashish.stash.StashApplication"
-)
-public final class _com_ashish_stash_StashApplication {
-}
